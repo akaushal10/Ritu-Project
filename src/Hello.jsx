24c9691deb68163abca3cbe702c951a2306244b1
Abhishek Koushal <akaushal451@gmail.com>
2021-06-15 18:58:01 +0530

    class component

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-const Hello = (props)=>{
-    // local vairblae => state
-    const {userName,age} = props
-    // Object Desturing
-    return (
-        <div>
-            <h1>
-                Hello {userName}: {age}
-            </h1>
-            <Input/>
-        </div>
-    )
-}
+import React from "react";
+const Hello = (props) => {
+  // local vairblae => state
+  const { userName, age } = props;
+  // Object Desturing
+//   const friends = [{name:"Neha",mobile:9743827483},{name:"Ingle",mobile:83433827483}]
+//   const [neha,ingle] = friends
+    const nehaDetails = {myname:"Neha",mobile:9743827483,friend:"Ritu"}
+    // const mobile =nehaDetails.mobile 
+    // const myname = nehaDetails.myname
+    const {myname,mobile} = nehaDetails
+  return (
+    <div>
+      <h1>
+        Hello {userName}: {age}
+        {mobile}
+        {myname}
+      </h1>
+    </div>
+  );
+};
 export default Hello;
--- src/HelloIngle.jsx
+++ src/HelloIngle.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Counter } from "./Counter";
 const HelloIngle = () => {
   return (
     <>
-      <div>
+      <div style={{ border: "2px solid black" }}>
         <h1>Hello Ingle</h1>
+        <Counter />
       </div>
     </>
   );
